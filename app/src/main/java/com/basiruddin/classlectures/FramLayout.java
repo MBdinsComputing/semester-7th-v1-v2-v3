ba55e19363ffd3808ebbb647720e0aa62b28715f
Basir uddin <basiru004@gmail.com>
2020-11-06 09:35:26 -0800

    version 1.1

---
+++ app/src/main/java/com/basiruddin/classlectures/FramLayout.java
@@ -0,0 +1,14 @@
+package com.basiruddin.classlectures;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FramLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fram_layout);
+    }
+}
---
+++ app/src/main/java/com/basiruddin/classlectures/OtherLayouts.java
@@ -0,0 +1,14 @@
+package com.basiruddin.classlectures;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+class OtherLayouts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid_layouts);
+    }
+}
